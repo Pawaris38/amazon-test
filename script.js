b556dc05e1301bddbf4fa3242dbafce7349b3517
goojeff999 <goojeff999@gmail.com>
2021-10-06 18:54:13 +0700

    Second

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const arrowleft = document.getElementById("arrow-left");
 const arrowright = document.getElementById("arrow-right");
 
-arrowleft.addEventListener('click', function(){
-    document.body.style.backgroundImage = "url(bg/bg6.jpg)";
-});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+ 
+  var slides = document.getElementsByClassName("slide");
+  
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+}
+
+ arrowleft.addEventListener('click', function(){
+    plusSlides(-1);
+ });
 
 arrowright.addEventListener('click', function(){
-    document.body.style.backgroundImage = "url(bg/bg2.jpg)";
+    plusSlides(1);
 });
